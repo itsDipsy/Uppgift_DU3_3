69a83da6f66abcf1b1fcb6ee9e7a9096e44dbc37
itsDipsy <elliot.collinszcv@gmail.com>
2023-03-01 12:55:12 +0100

    Update login_register.js

--- js/login_register.js
+++ js/login_register.js
@@ -1,7 +1,7 @@
 function init_login_register() {
     let login_register_component = document.createElement("div");
     login_register_component.innerHTML = `
-   
+    
    `;
 
     document.querySelector("main").appendChild(login_register_component);
