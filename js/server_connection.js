a7cdf8d85fc701109187569285a046c01c9bf34f
itsDipsy <elliot.collinszcv@gmail.com>
2023-03-02 13:34:10 +0100

    Update server_connection.js

--- js/server_connection.js
+++ js/server_connection.js
@@ -6,6 +6,9 @@ async function server_connection(request) {
         let response = await fetch(request);
         console.log(response);
         switch (response.status) {
+            case 200:
+                register_complete_or_not(response.ok)
+                break;
             case 418:
 
                 ima_teapot();
@@ -16,25 +19,25 @@ async function server_connection(request) {
                 break;
 
             case 409:
-                let register_reject_dom = document.createElement("div");
-                register_reject_dom.classList.add("fetch_await_dom");
-                register_reject_dom.innerHTML = `
+
+                let taken_name_dom = document.createElement("div");
+                taken_name_dom.classList.add("fetch_await_dom");
+                taken_name_dom.innerHTML = `
                 <div>
                     <h2>That name is already taken</h2>
                     <button class="exit">Exit</button>
                 </div>
                 `;
-                document.body.appendChild(register_reject_dom);
+                document.body.appendChild(taken_name_dom);
                 document.querySelector(".exit").addEventListener("click", () => {
-                    register_reject_dom.remove();
+                    taken_name_dom.remove();
                 })
                 break;
         }
 
         if (is_login_or_register_bool() === "register") {
             // hära ska wrong domen fucntionen vara
-            reset_error_dom(); // Denna if sats gör så att det inte poppar up en wrong message som i login om allt är tomt eller fel
-            register_complete_or_not(response.ok); // gör hela complete dom:en eller reject dom:en
+            reset_error_dom(); // Denna if sats gör så att det inte poppar up en wrong message som i login om allt är tomt eller fel// gör hela complete dom:en eller reject dom:en
         }
 
 
