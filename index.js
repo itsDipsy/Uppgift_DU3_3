e2545d131b171457544bc9b31cfc90e8e2dc6fd4
itsDipsy <elliot.collinszcv@gmail.com>
2023-03-02 12:46:31 +0100

    klart

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-init_login_register();
+console.log(localStorage);
+if (localStorage.length === 1) {
+    init_quiz_component(localStorage.getItem("user_logged_in"));
+}
+else {
+    init_login_register();
+}
